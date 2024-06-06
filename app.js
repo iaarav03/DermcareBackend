539f42d80c1aa38a679ddbb972e3f3e1b1984c45
iaarav03 <aarav.21ug2059@iiitranchi.ac.in>
2024-06-06 16:45:07 +0530

    frontend

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import appointmentRouter from "./router/appointmentRouter.js";
 
 const app=express();
 config({path: "./config/config.env"});
-const allowedOrigins = ['http://localhost:1234', 'https://your-frontend-domain.com'];
+const allowedOrigins = ['http://localhost:1234', 'https://derm-care-new-frtontend-iwvdrbblw-iaarav03s-projects.vercel.app'];
 
 const corsOptions = {
   origin: function (origin, callback) {
